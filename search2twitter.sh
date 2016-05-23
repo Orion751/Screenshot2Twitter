0f3ebc765afe67b5878647442b4bc43001417201
Orion751 <Orion751@users.noreply.github.com>
2016-05-23 16:01:35 -0400

    Started pseudocode

--- search2twitter.sh
+++ search2twitter.sh
@@ -1,2 +1,15 @@
 #!/bin/bash
 
+: '
+Pseudocode
+
+search=first line of searches
+
+Move $search to the end of oldSearches
+
+Create a screenshot with $search
+
+Send the screenshot to Twitter
+
+'
+
